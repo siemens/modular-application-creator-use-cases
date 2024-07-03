ee6f3f4b5327f91bd7864416398e81f21187e2cf
Fabian Zacharias <fabian.zacharias@siemens.com>
2024-07-03 15:39:06 +0200

    feat: add SCL-code with IF and ELSE

--- MAC_use_cases/Model/UseCases/IntegrateLibraries.cs
+++ MAC_use_cases/Model/UseCases/IntegrateLibraries.cs
@@ -1,4 +1,5 @@
-﻿using Siemens.Automation.ModularApplicationCreator.Tia.Openness;
+using Siemens.Automation.ModularApplicationCreator.Tia.Helper.Create_XML_Block.XmlBlocks.BlockFrames;
+using Siemens.Automation.ModularApplicationCreator.Tia.Openness;
 
 namespace MAC_use_cases.Model.UseCases
 {
@@ -18,6 +19,21 @@ namespace MAC_use_cases.Model.UseCases
         public static void CreateInstanceDB(MAC_use_casesEM module, FBMasterCopy masterCopy, string instanceName, BlockGroup target)
         {
             DataBlock instDB = module.ResourceManagement.CreateInstanceDb(masterCopy, instanceName, target.Blocks);
+        }
+
+
+        /// <summary>
+        /// This function can also creates an instance DB in the target folder (folder under program blocks)
+        /// \image html CreateInstanceDB_via_XmlInstDB.png
+        /// </summary>
+        /// <param name="module">The Module</param>
+        /// <param name="masterCopy">A master copy of the block you want to create an instance DB of</param>
+        /// <param name="instanceName">Name of the instance</param>
+        /// <param name="target">The folder under program blocks in which the DB is created</param>
+        public static void CreateInstanceDB_via_XmlInstDB(MAC_use_casesEM module, FBMasterCopy masterCopy, string instanceName, BlockGroup target, PlcDevice m_plcDevice)
+        {
+            XmlInstDB instance_DB_of_MAC_use_casesFB = new XmlInstDB(instanceName, masterCopy.Name);
+            instance_DB_of_MAC_use_casesFB.GenerateXmlBlock(m_plcDevice, target.Blocks);
         }
     }
 }
