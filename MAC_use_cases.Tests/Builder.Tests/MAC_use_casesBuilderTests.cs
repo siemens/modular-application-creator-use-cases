981bbf5f81ad24f5bccdebd3120e7c234f07327a
Fabian Zacharias <109723090+fabianzacharias@users.noreply.github.com>
2025-10-29 11:22:22 +0100

    feat: Update to MAC20.1.6 and TIAV20

--- MAC_use_cases.Tests/Builder.Tests/MAC_use_casesBuilderTests.cs
+++ MAC_use_cases.Tests/Builder.Tests/MAC_use_casesBuilderTests.cs
@@ -1,9 +1,7 @@
-﻿using MAC_use_cases.Model.UseCases;
-using MAC_use_cases.Tests.TestEnvironment;
 using NUnit.Framework;
-using Siemens.Automation.ModularApplicationCreator.Tia.Helper.Create_XML_Block;
-using Siemens.Automation.ModularApplicationCreator.Tia.Openness;
-using Siemens.Automation.ModularApplicationCreator.Tia.Openness.SoftwareUnit;
+
+using MAC_use_cases.Tests.TestEnvironment;
+
 using Siemens.ModularApplicationCreator.Testenvironment.TestCategoryAttributes;
 using Siemens.ModularApplicationCreator.Testenvironment.UnitTestBaseClasses.Enums;
 
@@ -14,33 +12,24 @@ namespace MAC_use_cases.Tests.Builder.Tests
         protected string LanguageSetting =>
             this.Get(() => ProjectOpenness.LanguageSettings.EditingLanguage.Culture.Name, NunitTestContext.TestCase);
 
-        
 
+[Test, MacFunctionTest]
+public void GenerateOBMainTest()
+{
+    //Test could look like the following
 
-        [Test, MacFunctionTest]
-        public void GenerateOBMainTest()
-        {
-            //Test could look like the following
+    //GenericBlockCreation.GenerateOB_Main("MyMultiInstanceDBName", EquipmentModule, LanguageSetting, PlcDeviceMacSimulated);
+}
 
-            //GenericBlockCreation.GenerateOB_Main("MyMultiInstanceDBName", EquipmentModule, LanguageSetting, PlcDeviceMacSimulated);
-        }
+[Test, MacFunctionTest]
+public void GenerateS120Test()
+{
+    //A test for the hardware generation could look like the following.
+    //Necessary to create the openness objects is to call the function GenerateHardware after configuration of the MAC objects.
 
-        [Test, MacFunctionTest]
-        public void GenerateS120Test()
-        {
-            //A test for the hardware generation could look like the following.
-            //Necessary to create the openness objects is to call the function GenerateHardware after configuration of the MAC objects.
+    //HardwareGeneration.GenerateS120(EquipmentModule, "Test", "Test");
+    //GenerateHardware();
+}
 
-            //HardwareGeneration.GenerateS120WithMasterCopy(EquipmentModule, "Test", "Test");
-            //GenerateHardware();
-        }
-
-        [Test, MacFunctionTest]
-        public void GenerateProgramBlock_SCL_in_SW_unit()
-        {
-          
-            var _softwareUnit = SoftwareUnits.GetOrCreateSoftwareUnit(PlcDeviceMacSimulated, "MyUnit", EquipmentModule, "Namesapce_example");
-            GenericBlockCreation.CreateFunctionBlockInSoftwareUnit(_softwareUnit, "MyFb_SCL3", ProgrammingLanguage.SCL, PlcDeviceMacSimulated);
-        }
-    }
+}
 }
