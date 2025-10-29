5dbcbac971c7b8945552bf6a7f2097f6579ee0b3
Fabian Zacharias <109723090+fabianzacharias@users.noreply.github.com>
2025-10-29 11:25:23 +0100

    feat: Update to MAC20.1.6 and TIAV20 (#82)

--- MAC_use_cases.Tests/TestEnvironment/MacFunctionTestBaseMAC_use_cases.cs
+++ MAC_use_cases.Tests/TestEnvironment/MacFunctionTestBaseMAC_use_cases.cs
@@ -1,58 +1,40 @@
-﻿using System.Reflection;
+using System.Reflection;
+
+using NUnit.Framework;
+
 using MAC_use_cases.Model;
 using MAC_use_cases.TiaImports;
-using NUnit.Framework;
+
 using Siemens.ModularApplicationCreator.Testenvironment;
 using Siemens.ModularApplicationCreator.Testenvironment.UnitTestBaseClasses.Enums;
+
+
 
 namespace MAC_use_cases.Tests.TestEnvironment
 {
     public class MacFunctionTestBaseMAC_use_cases : MacFunctionTestBase<MAC_use_casesEM>
     {
-        protected ResourceManagement ResourceManagement
-        {
-            get => Get(() => new ResourceManagement(), NunitTestContext.TestCase);
-        }
-
-        protected override string DirectoryNameOfViewTestResources
-        {
-            get => Get(() => "MAC_use_cases.Tests.Resources");
-        }
-
-        protected override string NameOfTiaZapFileFromResources
-        {
-            get => Get(() => "MacTestenvironmentBaseProject.zap19");
-        }
-
-        protected override Assembly Assembly
-        {
-            get => Get(() => typeof(MacFunctionTestBaseMAC_use_cases).Assembly);
-        }
-
-        protected override string PlcName
-        {
-            get => Get(() => "S7-1500/ET200MP station_1");
-        }
-
-        protected override bool AttachToOpenProject => true;
-
-        protected override string PlcCPUName
-        {
-            get => Get(() => "TestenvironmentPLC");
-        }
-
-        protected override string OrderNumberPLC
-        {
-            get => Get(() => "");
-        }
-
         [TearDown]
         public override void Teardown()
         {
         }
 
+protected ResourceManagement ResourceManagement => this.Get(() => new ResourceManagement(), NunitTestContext.TestCase);
+
         protected override void InitializeModuleClassesForTesting()
         {
         }
+
+
+        protected override string DirectoryNameOfViewTestResources =>
+            this.Get(() => "MAC_use_cases.Tests.Resources");
+
+protected override string NameOfTiaZapFileFromResources => this.Get(() => "MacTestenvironmentBaseProject.zap20");
+        protected override Assembly Assembly => this.Get(() => typeof(MacFunctionTestBaseMAC_use_cases).Assembly);
+
+        protected override string PlcName => this.Get(() => "S7-1500/ET200MP station_1");
+        protected override string PlcCPUName => this.Get(() => "TestenvironmentPLC");
+        protected override string OrderNumberPLC => this.Get(() => "");
     }
+
 }
